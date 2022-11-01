ee8596bde4e32c7d79e820dc0642ccf149aea78b
Denis Avvakumov <denisavvakumov@gmail.com>
2022-11-01 12:18:09 +0400

    NDEV-792: Add ClickHouse client, create tables

--- v2-advanced/clickhouse/create_update_slot_mv.sql
+++ v2-advanced/clickhouse/create_update_slot_mv.sql
@@ -1,4 +1,4 @@
-CREATE MATERIALIZED VIEW update_slot_queue_mv to update_slot
+CREATE MATERIALIZED VIEW IF NOT EXISTS update_slot_queue_mv to update_slot
 AS
     SELECT slot, parent, slot_status
     FROM update_slot_queue;
