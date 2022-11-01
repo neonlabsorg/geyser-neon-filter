f07678f9e9c8ab8d4bd702bd3363bc5b04504ab4
Denis Avvakumov <denisavvakumov@gmail.com>
2022-11-01 15:37:36 +0400

    NDEV-792: Add timestamp for update_slot table

--- v2-advanced/clickhouse/create_update_slot.sql
+++ v2-advanced/clickhouse/create_update_slot.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS update_slot
 (
     slot UInt64,
     parent Nullable(UInt64) default 0,
-    slot_status Enum('Processed' = 1, 'Rooted' = 2, 'Confirmed' = 3)
+    slot_status Enum('Processed' = 1, 'Rooted' = 2, 'Confirmed' = 3),
+    timestamp DateTime64
 )
-ENGINE = MergeTree
-PRIMARY KEY(slot);
+ENGINE = ReplicatedMergeTree
+PRIMARY KEY(slot)
+ORDER BY (slot);
--- v2-advanced/clickhouse/create_update_slot_mv.sql
+++ v2-advanced/clickhouse/create_update_slot_mv.sql
@@ -1,4 +1,4 @@
 CREATE MATERIALIZED VIEW IF NOT EXISTS update_slot_queue_mv to update_slot
 AS
-    SELECT slot, parent, slot_status
+    SELECT slot, parent, slot_status, now64() as timestamp
     FROM update_slot_queue;
