c8367a7fdaed7948fa7501a90a99182de7788e90
Denis Avvakumov <denisavvakumov@gmail.com>
2022-11-25 17:23:31 +0400

    NDEV-1007: Move build info logic to separate module

--- v1-simple/src/main.rs
+++ v1-simple/src/main.rs
@@ -1,3 +1,4 @@
+mod build_info;
 mod config;
 mod consumer;
 mod db;
@@ -5,7 +6,7 @@ mod filter;
 
 use std::sync::Arc;
 
-use crate::consumer::consumer;
+use crate::{build_info::get_build_info, consumer::consumer};
 use clap::{Arg, Command};
 use config::{env_build_config, FilterConfig};
 use crossbeam_queue::SegQueue;
@@ -18,38 +19,6 @@ use fast_log::{
 use filter::filter;
 use log::{error, info};
 use tokio::fs;
-
-build_info::build_info!(fn build_info);
-
-fn get_build_info() -> String {
-    let build_info = build_info();
-    let mut build_info_string = String::new();
-    if let Some(version_control) = &build_info.version_control {
-        if let Some(git_info) = version_control.git() {
-            build_info_string = format!(
-                "{}, branch {:?}, commit {}\nbuilt with Rust {}, opt level {}, at {}",
-                build_info.crate_info.name,
-                git_info
-                    .branch
-                    .as_ref()
-                    .unwrap_or(&"Unknown branch".to_string()),
-                git_info.commit_id,
-                build_info.compiler.version,
-                build_info.optimization_level,
-                build_info.timestamp.format("%d/%m/%Y %H:%M %:z"),
-            );
-        }
-    } else {
-        build_info_string = format!(
-            "{} built with Rust {}, opt level {}, at {}",
-            build_info.crate_info.name,
-            build_info.compiler.version,
-            build_info.optimization_level,
-            build_info.timestamp.format("%d/%m/%Y %H:%M %:z"),
-        );
-    }
-    build_info_string
-}
 
 async fn run(config: FilterConfig) {
     let logger: &'static Logger = fast_log::init(Config::new().console().file_split(
