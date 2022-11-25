9a254c5cad96c62e9225ecad582612a562c77577
Denis Avvakumov <denisavvakumov@gmail.com>
2022-11-25 10:44:40 +0400

    NDEV-1007: Show additional build information

--- v1-simple/src/main.rs
+++ v1-simple/src/main.rs
@@ -16,8 +16,40 @@ use fast_log::{
     Config, Logger,
 };
 use filter::filter;
-use log::error;
+use log::{error, info};
 use tokio::fs;
+
+build_info::build_info!(fn build_info);
+
+fn get_build_info() -> String {
+    let build_info = build_info();
+    let mut build_info_string = String::new();
+    if let Some(version_control) = &build_info.version_control {
+        if let Some(git_info) = version_control.git() {
+            build_info_string = format!(
+                "{}, branch {:?}, commit {}\nbuilt with Rust {}, opt level {}, at {}",
+                build_info.crate_info.name,
+                git_info
+                    .branch
+                    .as_ref()
+                    .unwrap_or(&"Unknown branch".to_string()),
+                git_info.commit_id,
+                build_info.compiler.version,
+                build_info.optimization_level,
+                build_info.timestamp.format("%d/%m/%Y %H:%M %:z"),
+            );
+        }
+    } else {
+        build_info_string = format!(
+            "{} built with Rust {}, opt level {}, at {}",
+            build_info.crate_info.name,
+            build_info.compiler.version,
+            build_info.optimization_level,
+            build_info.timestamp.format("%d/%m/%Y %H:%M %:z"),
+        );
+    }
+    build_info_string
+}
 
 async fn run(config: FilterConfig) {
     let logger: &'static Logger = fast_log::init(Config::new().console().file_split(
@@ -27,6 +59,8 @@ async fn run(config: FilterConfig) {
         LogPacker {},
     ))
     .expect("Failed to initialize fast_log");
+
+    info!("{}", get_build_info());
 
     let config = Arc::new(config);
     let db_queue: Arc<SegQueue<DbAccountInfo>> = Arc::new(SegQueue::new());
@@ -60,6 +94,8 @@ async fn main() {
                 .help("Sets the path to the config file"),
         )
         .get_matches();
+
+    println!("{}", get_build_info());
 
     if let Some(config_path) = app.get_one::<String>("config") {
         println!("Trying to read the config file: {config_path}");
